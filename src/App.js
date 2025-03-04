15035de92d2dec49e85db44ee1f8c13daca673c1
web-source-dev <muhammadnouman72321@gmail.com>
2025-03-04 14:40:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import VendorRegistration from './components/VendorForm';
 import BuyerForm from './components/BuyerForm';
 import Login from './components/Login';
@@ -11,14 +11,15 @@ import BuyerDashboard from './Dashboard/BuyerDashboard/BuyerDashboard';
 const App = () => {
   return (
     <Router>
-      <div style={{padding:"1rem" ,backgroundColor: "var(--background-color)", color: "var(--text-color)" }}>
+      <div style={{ padding: "1rem", backgroundColor: "var(--background-color)", color: "var(--text-color)" }}>
         <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/admin-dashboard" element={<AdminDashboard/>} />
-        <Route path="/buyer-dashboard" element={<BuyerDashboard/>} />
-        <Route path="/vendor-dashboard" element={<VendorDashboard/>} />
-        <Route path="/vendor" element={<VendorRegistration />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
+          <Route path="/vendor-dashboard" element={<VendorDashboard />} />
+          <Route path="/vendor" element={<VendorRegistration />} />
           <Route path="/vendor/:email" element={<VendorRegistration />} />
           <Route path="/buyer" element={<BuyerForm />} />
           <Route path="/buyer/:email" element={<BuyerForm />} />
